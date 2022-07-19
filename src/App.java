481239efd525f474fd666194332b39c95e0e2187
Isaque Coelho <isaquecoelho.dj@gmail.com>
2022-07-19 19:29:18 -0300

    UTF-8

--- src/App.java
+++ src/App.java
@@ -7,12 +7,12 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
-    private static JsonParser jsonParser;
 
     public static void main(String[] args) throws Exception {
 
         // Fazzer uma conexão HTTP buscar os tops 250 filmes
-        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_8pw5xuzg";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -20,19 +20,26 @@ public class App {
         String body = response.body();
 
         // extrair só os dadod que interesssam (título, poster, classificação)
+
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
         // exibir e manipular os dados
-        
+
         for (Map<String, String> filme : listaDeFilmes) {
+
             System.out.println("\u001b[37m" + "Título: " + "\u001b[1m" + filme.get("title") + "\u001b[1m");
             System.out.println(filme.get("image"));
-            System.out.println("\u001b[37m \u001b[45m" + "Classificação: " + filme.get("imDbRating" ) + " \u001b[m" +);
+            System.out.println("\u001b[37m \u001b[45m" + "Classificação: " + filme.get("imDbRating") + " \u001b[m");
+            int classificacao = (int) Float.parseFloat(filme.get("imDbRating"));
+            String stars = "\u2B50";
+
+            for (int i = 1; i < classificacao; i++) {
+                stars += "\u2B50";
+            }
+
+            System.out.println(stars);
             System.out.println();
-
         }
-
     }
-
 }
